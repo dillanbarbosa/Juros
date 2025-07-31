ca0628c77616f8f348a05cb62163b2699e25bb5a
dillanbarbosa <dillan.invencao@escola.pr.gov.br>
2025-07-31 08:46:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcula(){
+   
+    let a = parseFloat(document.getElementById('capital').value);
+    let b = parseFloat(document.getElementById('taxa').value);
+    let c = parseFloat(document.getElementById('tempo').value);
+    let m = parseFloat(document.getElementById('montante').value);
+    b = b/100
+    let resultado = a * b * c;
+    document.getElementById("resultado").textContent = 'seu juros é: ' + resultado;
+    m = a + resultado;
+    document.getElementById("resultado").textContent = 'seu montante é ' + m;
+}
